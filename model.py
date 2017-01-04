9a084251718160062fc8735ea549c739b889c1cc
Mornor <cesliens@gmail.com>
2017-01-04 09:40:17 +0100

    Use utils.py methods

--- model.py
+++ model.py
@@ -2,57 +2,17 @@
 import os
 import argparse
 import json
-import csv
+import utils
 import numpy as np
-import matplotlib.image as mpimg
 from sklearn import cross_validation
 from keras.models import Sequential, model_from_json
 from keras.layers import Dense, Dropout, Flatten, Lambda
 from keras.layers.convolutional import Convolution2D
 
-# Usefull constants
-DATA_PATH = "driving_log.csv"
-
-# Load the CSV names into memory as tuples
-# Center image: np.array(csv[:,0])
-# Left image: np.array(csv[:,1])
-# Right image: np.array(csv[:,2])
-# Sterring angle: np.array(csv[:,3], dtype=float)
-# (160, 320, 3)
-def load_data():
-	with open(DATA_PATH, 'r') as file:
-		reader = csv.reader(file)
-		file.readline() # Skip headers
-		data = np.array([row for row in reader])
-	return data
-
 # Flip image and inverse the steering angle
 def generate_more_data(img, steering_angle):
 	print(img.size[0])
 	print(img.size[1])
-
-
-# Process a single image
-# Normalize data from 0-255 to -1 - 1
-# Scale down image to 32 * 16 (from 160, 320, 3)
-def process_img(img):
-	img = img[::10,::10].copy()
-	return img/127.5 - 1.
-
-# Process images from input
-def process_image(data):
-	# Load the images into np array of shape (len(data), original_height/5, original_width/20, original_channel)
-	center_images = np.zeros((len(data), 16, 32, 3), dtype=float)
-	left_images = np.zeros((len(data), 16, 32, 3), dtype=float)
-	right_images = np.zeros((len(data), 16, 32, 3), dtype=float)
-	for i in range(8000, len(data)):
-		center_images[i] = process_img(mpimg.imread(data[:,0][i].strip()))
-		left_images[i] = process_img(mpimg.imread(data[:,1][i].strip()))
-		right_images[i] = process_img(mpimg.imread(data[:,2][i].strip()))
-	
-	#print(center_images.shape)
-	#test_images = np.array([mpimg.imread(path_folder + "/" + file) for file in filenames])
-	return center_images, left_images, right_images
 
 # Split data into training, normalization and test set
 def split_input(X, y):
@@ -127,9 +87,9 @@ def get_model():
 	return model
 
 # Load Data
-data = load_data()
+data = utils.load_data()
 # Pre-Process data
-center_images, left_images, right_images = process_image(data)
+center_images, left_images, right_images = utils.process_image(data)
 # Split data into training, test and validation set
 y_data = np.array(data[:,3], dtype=float)
 X_train, y_train, X_val, y_val = split_input(center_images, y_data)
