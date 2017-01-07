617778da730aa63f7bff15b058591c16e1c2ff95
Mornor <cesliens@gmail.com>
2017-01-07 16:55:01 +0100

    Add a script for quick setup on AWS

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set +x
+
+# Get data from Udacity
+wget https://d17h27t6h515a5.cloudfront.net/topher/2016/December/584f6edd_data/data.zip
+
+# Unzip it
+unzip data.zip
+
+# Move to current folder 
+mv data/*
