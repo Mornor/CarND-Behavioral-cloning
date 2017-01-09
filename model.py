bb52a081c4adff03cb3d5c5b4bbe530ecb6901ba
Mornor <cesliens@gmail.com>
2017-01-09 18:38:04 +0000

    Push changes

--- model.py
+++ model.py
@@ -126,13 +126,13 @@ data = utils.flip_center_images(data)
 # Randomly Shuffle data
 np.random.shuffle(data)
 # Pre-Process data
-test_images = utils.process_images(data, nvidia=True)
+test_images = utils.process_images(data, nvidia=False)
 # Split data into training, test and validation set
 y_data = np.array(data[:,1], dtype=float)
 X_train, y_train, X_val, y_val = split_into_sets(test_images, y_data)
 # Get the model
-model = get_model(nvidia=True)
+model = get_model(nvidia=False)
 # Train the model
-trained_model = train(model, X_train, y_train, X_val, y_val, 2048, 5) # To do: handle the case where the batch_size (sample_per_epochs) is not a factor of len(data)
+trained_model = train(model, X_train, y_train, X_val, y_val, 256, 5) # To do: handle the case where the batch_size (sample_per_epochs) is not a factor of len(data)
 # Save it
 utils.save_model(trained_model)
