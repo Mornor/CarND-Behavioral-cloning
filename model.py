95201b5addf25515bd3a377ded48cc3365cdab60
Mornor <cesliens@gmail.com>
2017-01-10 21:13:10 +0100

    Indent

--- model.py
+++ model.py
@@ -117,34 +117,34 @@ def get_model(nvidia):
 	'''
 
 	model.add(Convolution2D(32, 5, 5, input_shape=(32, 64, 3), subsample=(2, 2), border_mode="same"))
-    model.add(ELU())
+	model.add(ELU())
 
-    # layer 2 output shape is 15x15x16
-    model.add(Convolution2D(16, 3, 3, subsample=(1, 1), border_mode="valid"))
-    model.add(ELU())
-    model.add(Dropout(.4))
-    model.add(MaxPooling2D((2, 2), border_mode='valid'))
+	# layer 2 output shape is 15x15x16
+	model.add(Convolution2D(16, 3, 3, subsample=(1, 1), border_mode="valid"))
+	model.add(ELU())
+	model.add(Dropout(.4))
+	model.add(MaxPooling2D((2, 2), border_mode='valid'))
 
-    # layer 3 output shape is 12x12x16
-    model.add(Convolution2D(16, 3, 3, subsample=(1, 1), border_mode="valid"))
-    model.add(ELU())
-    model.add(Dropout(.4))
+	# layer 3 output shape is 12x12x16
+	model.add(Convolution2D(16, 3, 3, subsample=(1, 1), border_mode="valid"))
+	model.add(ELU())
+	model.add(Dropout(.4))
 
-    # Flatten the output
-    model.add(Flatten())
+	# Flatten the output
+	model.add(Flatten())
 
-    # layer 4
-    model.add(Dense(1024))
-    model.add(Dropout(.3))
-    model.add(ELU())
+	# layer 4
+	model.add(Dense(1024))
+	model.add(Dropout(.3))
+	model.add(ELU())
 
-    # layer 5
-    model.add(Dense(512))
-    model.add(ELU())
+	# layer 5
+	model.add(Dense(512))
+	model.add(ELU())
 
-    # Finally a single output, since this is a regression problem
-    model.add(Dense(1))
-	
+	# Finally a single output, since this is a regression problem
+	model.add(Dense(1))
+
 	# Use the Adam optimizer to optimize the mean squared error
 	model.compile(optimizer=adam, loss="mse")	
 
